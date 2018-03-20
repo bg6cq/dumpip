2c806fa5106a192d5d1026bae7946f8acd1bceed
Zhang Huanjie <james@ustc.edu.cn>
2018-03-20 16:33:38 +0800

    no sock.h

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
 dumpip: dumpip.c
-	gcc -o dumpip dumpip.c -Wall
+	gcc -Wall -g -o dumpip dumpip.c
+
+indent: dumpip.c
+	indent dumpip.c -nbad -bap -nbc -bbo -hnl -br -brs -c33 -cd33 -ncdb -ce -ci4  \
+		-cli0 -d0 -di1 -nfc1 -i8 -ip0 -l100 -lp -npcs -nprs -npsl -sai \
+		-saf -saw -ncs -nsc -sob -nfca -cp33 -ss -ts8 -il1
--- dumpip.c
+++ dumpip.c
@@ -1,96 +1,113 @@
+/* 
+ * dumpip  - A simple TCP server to dump client IP
+ * usage: dumpip <port> [ logfile ]
+ */
+
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
-#include <sys/types.h>
 #include <time.h>
-#include <endian.h>
-#include <netinet/if_ether.h>
-#include <netinet/ip.h>
-#include <netinet/udp.h>
-#include <netinet/tcp.h>
-#include <getopt.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-#define MAXLEN 16384 
+#define BUFSIZE 1024
 
-int debug=0;
-
-#include "sock.h"
-
-char * INET_NTOA(unsigned long int ip)
-{ 	static char buf[100];
- 	sprintf(buf,"%d.%d.%d.%d",
- 	(unsigned int)((ip>>24)&0xff), (unsigned int)((ip>>16)&0xff), (unsigned int)((ip>>8)&0xff), (unsigned int)((ip)&0xff));
- 	return buf;
-}
-
-char * INET_NTOA2(unsigned long int ip)
-{ 	static char buf[100];
- 	sprintf(buf,"%d.%d.%d.%d",
- 	(unsigned int)((ip>>24)&0xff), (unsigned int)((ip>>16)&0xff), (unsigned int)((ip>>8)&0xff), (unsigned int)((ip)&0xff));
- 	return buf;
-}
-
-void alarm_handler() {
-	fprintf(stderr,"holdtime expired, exit\n");
-	exit(0);
-}
-
-int do_dumpip(char *p, char *logf)
+void error(char *msg)
 {
-	unsigned char buf[MAXLEN];
-	int lf;
-	lf=Tcp_listen("0.0.0.0",p,NULL);
-	
-	while(1) {
-		int s;
-		int len;
-		FILE *fp;
-		struct sockaddr_in sa;
-		len=sizeof(sa);
-		s=Accept(lf,&sa,&len);
-		snprintf(buf,MAXLEN,"\n\n\nYour IP is %s\n\n\n\n",INET_NTOA(ntohl(sa.sin_addr.s_addr)));
-		write(s,buf,strlen(buf));
-		close(s);
-		fp=fopen(logf,"a");
-		time_t nowt;
-		struct tm * c_tm;
-		nowt=time(NULL);
-		c_tm = localtime(&nowt);
-		fprintf(fp,"%d-%02d-%02d %02d:%02d:%02d %s\n",
-			c_tm->tm_year+1900, c_tm->tm_mon+1,
-                	c_tm->tm_mday, c_tm->tm_hour,
-                	c_tm->tm_min, c_tm->tm_sec,
-			INET_NTOA(ntohl(sa.sin_addr.s_addr)));
-		fclose(fp);
-		fprintf(stderr,"%d-%02d-%02d %02d:%02d:%02d %s\n",
-			c_tm->tm_year+1900, c_tm->tm_mon+1,
-                	c_tm->tm_mday, c_tm->tm_hour,
-                	c_tm->tm_min, c_tm->tm_sec,
-			INET_NTOA(ntohl(sa.sin_addr.s_addr)));
+	perror(msg);
+	exit(1);
+}
+
+void daemon_init(void)
+{
+	int pid;
+	pid = fork();
+	if (pid == -1)
+		error("fork error!");
+	else if (pid > 0)
+		exit(0);
+
+	if (setsid() == -1)
+		error("setsid error!");
+
+	pid = fork();
+	if (pid == -1)
+		error("fork error!");
+	else if (pid > 0)
+		exit(0);
+	setsid();
+	close(0);
+	close(1);
+	close(2);
+}
+
+int main(int argc, char **argv)
+{
+	int listenfd;
+	int childfd;
+	int port;
+	socklen_t clientlen;
+	struct sockaddr_in serveraddr;
+	struct sockaddr_in clientaddr;
+	char buf[BUFSIZE];
+	int optval;
+
+	if (argc < 2) {
+		fprintf(stderr, "usage: %s <port> [ logfile ]\n", argv[0]);
+		exit(1);
 	}
-	return 0;
-}
 
-void usage()
-{
-	fprintf(stderr,"Usage:  dumpip port log_file\n");
-	exit(0);
-}
+	daemon_init();
 
-int main(int argc, char*argv[])
-{	int c;
-	signal(SIGALRM,alarm_handler);
-	if(argc!=3) usage();
-	while(1) {
-		int pid;
-		pid=fork();
-		if(pid==0) // child 
-			return do_dumpip(argv[1],argv[2]);
-		else if(pid==-1) // error
-			exit(0);
-		else 
-			wait(NULL);
-		sleep(10);
+	port = atoi(argv[1]);
+
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (listenfd < 0)
+		error("ERROR opening socket");
+
+	optval = 1;
+	setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval, sizeof(int));
+	bzero((char *)&serveraddr, sizeof(serveraddr));
+
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons((unsigned short)port);
+
+	if (bind(listenfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0)
+		error("ERROR on binding");
+
+	if (listen(listenfd, 5) < 0)
+		error("ERROR on listen");
+
+	clientlen = sizeof(clientaddr);
+	while (1) {
+		childfd = accept(listenfd, (struct sockaddr *)&clientaddr, &clientlen);
+		if (childfd < 0)
+			continue;
+
+		snprintf(buf, BUFSIZE, "\n\n\nYour IP is %s\n\nYour Port is %d\n\n",
+			 inet_ntoa(clientaddr.sin_addr), htons(clientaddr.sin_port));
+
+		write(childfd, buf, strlen(buf));
+
+		if (argc >= 3) {
+			FILE *fp;
+			fp = fopen(argv[2], "a");
+			time_t nowt;
+			struct tm *c_tm;
+			nowt = time(NULL);
+			c_tm = localtime(&nowt);
+			fprintf(fp, "%d-%02d-%02d %02d:%02d:%02d %s\n",
+				c_tm->tm_year + 1900, c_tm->tm_mon + 1,
+				c_tm->tm_mday, c_tm->tm_hour,
+				c_tm->tm_min, c_tm->tm_sec, inet_ntoa(clientaddr.sin_addr));
+			fclose(fp);
+		}
+
+		close(childfd);
 	}
 }
